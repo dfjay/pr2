212dafd3dfdc98e899a10fd13a57c7147ad4da64
dfjay <mail@dfjay.com>
2025-04-12 17:40:35 +0500

    Add pr6 and 7

--- main.go
+++ main.go
@@ -1,15 +1,76 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
+
+var (
+	httpRequestsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method", "path"},
+	)
+
+	httpRequestDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "Duration of HTTP requests",
+			Buckets: []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10},
+		},
+		[]string{"method", "path"},
+	)
+)
+
+func prometheusMiddleware() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		start := time.Now()
+		method := c.Method()
+		path := c.Path()
+
+		err := c.Next()
+
+		duration := time.Since(start).Seconds()
+		httpRequestsTotal.WithLabelValues(method, path).Inc()
+		httpRequestDuration.WithLabelValues(method, path).Observe(duration)
+
+		return err
+	}
+}
+
+func readFileUnsafe() {
+	data, err := ioutil.ReadFile("")
+	if err != nil {
+		fmt.Println("Ошибка чтения файла:", err)
+		return
+	}
+	fmt.Println(string(data))
+}
 
 func main() {
 	app := fiber.New()
+
+	app.Use(prometheusMiddleware())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello world")
 	})
 
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":2112", nil)
+	}()
+
 	app.Listen(":8080")
+
+	// pr7
+	readFileUnsafe()
 }
